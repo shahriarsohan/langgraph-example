15d579acfe582725765fd503ad282ce7945ba31c
Shahriar Sohan <sohanmock@gmail.com>
2025-02-28 01:14:13 +0600

    preparing for build

--- my_agent/utils/tools.py
+++ my_agent/utils/tools.py
@@ -1,3 +1,8 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
+import os
+
+from dotenv import load_dotenv
+
+os.environ["TAVILY_API_KEY"]  = os.getenv("TAVILY_API_KEY")
 
 tools = [TavilySearchResults(max_results=1)]
